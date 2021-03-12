1b73ed6eda0ec41a3f31ed6955b272533269bc05
A Little Cooing <TrentWu1998@gmail.com>
2021-03-12 15:53:55 +0800

    add FilePreview

---
+++ BlazorLinuxAdmin/Components/FilePreview.razor.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorLinuxAdmin.Components
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components;
+
+    public partial class FilePreview
+    {
+        [Parameter]
+        public FileInfo File { get; set; }
+
+        private string FileContent()
+        {
+            string fileContent = "";
+            using (FileStream fs = this.File.OpenRead())
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while(fs.Read(b, 0, b.Length) > 0)
+                {
+                    fileContent += temp.GetString(b);
+                }
+            }
+
+            return fileContent;
+        }
+    }
+}
